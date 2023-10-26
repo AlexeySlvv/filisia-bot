9c2034ccbdd46e0da4cd8005a5fcd1d40037012e
AlexeySlvv <aizdes2015@gmail.com>
2023-10-26 12:49:35 +0000

    move to Sber GigaChat

--- client.py
+++ client.py
@@ -1,9 +1,17 @@
+import time
+import json
+import os
+from typing import Literal
+
 from aiogram import types
-import g4f
 
 from create_bot import dp
+from gigachat import get_token, get_models, get_reply
 
-#from openaihosted import Completion
+
+t_key = Literal["access_token", "expires_at"]
+m_key = Literal["id", "object", "owned_by"]
+
 
 async def do_start(msg: types.Message):
     await msg.answer("Спросите меня о чём-нибудь.")
@@ -18,13 +26,43 @@ async def do_reply(msg: types.Message):
     if msg.text.startswith('/'):
         await msg.reply(text="Вопрос не должен начинаться с /")
 
+    resp = "Извините, сейчас я занята и не могу ответить. Попробуйте позже."
     try:
         msg1 = await msg.reply(text="Уже пишу. \N{feather}")
-        resp = await g4f.ChatCompletion.create_async(model='gpt-3.5-turbo', provider=g4f.Provider.FreeGpt, messages=[{"role": "user", "content": msg.text}])
+
+        token_path = os.path.join("/tmp", ".token.json")
+        token, expat = None, None
+        token_exist = os.path.exists(token_path)
+        if token_exist:
+            with open(token_path, mode="r") as t_in:
+                t: dict[t_key, str] = json.loads(t_in.read())
+                token, expat = t.get("access_token"), t.get("expires_at")
+
+        ts = time.time() * 1000
+        time_expired = bool(expat) and int(ts) > int(expat)
+
+        if not token_exist or time_expired or not token:
+            t: dict[t_key, str] = get_token()
+            token, expat = t.get("access_token"), t.get("expires_at")
+
+            if token is None and expat is None:
+                raise Exception("No access_token and expires_at")
+
+            with open(token_path, mode="w+") as t_out:
+                json.dump(t, t_out, ensure_ascii=False, indent=4)
+
+        models: list[dict[m_key, str]] = get_models(token=token).get("data")
+        if models:
+            # just using the very first model
+            model = models[0]["id"]
+            answ = get_reply(token=token, model=model, prompt=msg.text)
+            resp = answ["choices"][0]["message"]["content"] 
+
         await msg.reply(resp)
         await msg1.delete()
     except Exception as e:
-        await msg.reply(text="Ошибка: "+str(e))
+        await msg.reply("Извините, случилась внезапная оказия.")
+        await msg.answer(text=str(e))
 
 
 def register_client_handlers():
--- create_bot.py
+++ create_bot.py
@@ -5,12 +5,16 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import argparse
 
+import gigachat
+
 parser = argparse.ArgumentParser(description='Filisia telegram bot')
 parser.add_argument('-t', '--token', help='Filisia token file')
+parser.add_argument('-a', '--auth', help='Filisia auth file')
+
+args = parser.parse_args()
 
 # token
 try:
-    args = parser.parse_args()
     with open(args.token, mode='r') as t_f:
         TOKEN = t_f.read().strip()
         bot = Bot(token=TOKEN)
@@ -18,4 +22,13 @@ try:
         dp = Dispatcher(bot, storage=storage)
 except Exception as e:
     print('Token file error:', str(e))
+    exit()
+
+
+# auth
+try:
+    with open(args.auth, mode='r') as a_f:
+        gigachat.authority = a_f.read().strip()
+except Exception as e:
+    print('Authority file error:', str(e))
     exit()
