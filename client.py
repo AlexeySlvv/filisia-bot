659021b3fddd9b80f30d8a79f234581233359ca9
Alexey Solovev <solovev.a.a@niisvt.ru>
2023-10-26 16:10:57 +0300

    update client.py

--- client.py
+++ client.py
@@ -25,44 +25,47 @@ async def do_help(msg: types.Message):
 async def do_reply(msg: types.Message):
     if msg.text.startswith('/'):
         await msg.reply(text="Вопрос не должен начинаться с /")
+    else:
+        resp = "Извините, сейчас я занята и не могу ответить. Попробуйте позже."
+        try:
+            msg1 = await msg.reply(text="Уже пишу. \N{feather}")
 
-    resp = "Извините, сейчас я занята и не могу ответить. Попробуйте позже."
-    try:
-        msg1 = await msg.reply(text="Уже пишу. \N{feather}")
+            token_path = os.path.join("/tmp", ".token.json")
+            token, expat = None, None
+            token_exist = os.path.exists(token_path)
+            if token_exist:
+                with open(token_path, mode="r") as t_in:
+                    t: dict[t_key, str] = json.loads(t_in.read())
+                    token, expat = t.get("access_token"), t.get("expires_at")
 
-        token_path = os.path.join("/tmp", ".token.json")
-        token, expat = None, None
-        token_exist = os.path.exists(token_path)
-        if token_exist:
-            with open(token_path, mode="r") as t_in:
-                t: dict[t_key, str] = json.loads(t_in.read())
+            ts = time.time() * 1000
+            time_expired = bool(expat) and int(ts) > int(expat)
+
+            if not token_exist or time_expired or not token:
+                t: dict[t_key, str] = get_token()
                 token, expat = t.get("access_token"), t.get("expires_at")
 
-        ts = time.time() * 1000
-        time_expired = bool(expat) and int(ts) > int(expat)
+                if token is None and expat is None:
+                    raise Exception("No access_token and expires_at")
 
-        if not token_exist or time_expired or not token:
-            t: dict[t_key, str] = get_token()
-            token, expat = t.get("access_token"), t.get("expires_at")
+                with open(token_path, mode="w+") as t_out:
+                    json.dump(t, t_out, ensure_ascii=False, indent=4)
 
-            if token is None and expat is None:
-                raise Exception("No access_token and expires_at")
+            models: list[dict[m_key, str]] = get_models(token=token).get("data")
+            if models:
+                # just using the very first model
+                model = models[0]["id"]
+                answ = get_reply(token=token, model=model, prompt=msg.text)
+                resp = answ["choices"][0]["message"]["content"] 
 
-            with open(token_path, mode="w+") as t_out:
-                json.dump(t, t_out, ensure_ascii=False, indent=4)
-
-        models: list[dict[m_key, str]] = get_models(token=token).get("data")
-        if models:
-            # just using the very first model
-            model = models[0]["id"]
-            answ = get_reply(token=token, model=model, prompt=msg.text)
-            resp = answ["choices"][0]["message"]["content"] 
-
-        await msg.reply(resp)
-        await msg1.delete()
-    except Exception as e:
-        await msg.reply("Извините, случилась внезапная оказия.")
-        await msg.answer(text=str(e))
+            await msg.reply(resp)
+            await msg1.delete()
+        except Exception as e:
+            await msg.reply("Извините, случилась внезапная оказия.")
+            await msg.answer(text=str(e))
+        finally:
+            if msg1:
+                await msg1.delete()
 
 
 def register_client_handlers():
